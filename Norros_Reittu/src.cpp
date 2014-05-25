5ec8bd65b2a97ea5cd9cff0874cf1b9d5def5e4f
Anuj Agarwal <anujagarwal464@gmail.com>
2014-05-25 12:46:56 +0530

    Added Norros_Reittu graph

---
+++ Norros_Reittu/src.cpp
@@ -0,0 +1,104 @@
+/*
+ * Norros-Reittu graph
+ *
+ * Consider Graph G (n, W) with n vertices and weights W = (W1, W2, ..., Wn) >= 0
+ * In this model, an edge between two vertices i and j is independently present with probability
+ *             P (i, j) = 1 – exp (-WiWj/ summation (Wk))
+ *
+ */
+
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <algorithm>
+
+#include <ogdf/basic/Graph.h> 
+#include <ogdf/basic/NodeArray.h>
+#include <ogdf/basic/geometry.h>
+
+using std::cout;
+using std::minstd_rand;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
+using namespace ogdf;
+
+//! Creates a Norros-Reittu Graph. 
+/**
+* @param G is assigned the generated graph.
+* @param n is the number of nodes of the generated graph.
+* @param weights is Array of weight of each node.
+*/
+void norrosReittu(Graph &G, int n, Array<double> &weights) {
+	G.clear();
+	if (n == 0) return;
+
+	minstd_rand rng(randomSeed());
+	uniform_real_distribution<> dist(0, 1);
+
+	Array<node> v(n);
+	double Wk = 0.0;
+
+	for (int i = 0; i < n; i++) {
+		v[i] = G.newNode();
+		Wk += weights[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			double p = 1.0 - exp(-(weights[i] * weights[j]) / Wk);
+			if (dist(rng) <= p) {
+				G.newEdge(v[i], v[j]);
+			}
+		}
+	}
+
+}
+
+//! Creates a Norros-Reittu Graph with random weights. 
+/**
+* @param G is assigned the generated graph.
+* @param n is the number of nodes of the generated graph.
+*/
+void norrosReittuRandomWeights(Graph &G, int n) {
+	G.clear();
+	if (n == 0) return;
+
+	minstd_rand rng(randomSeed());
+	uniform_real_distribution<> dist(0, 1);
+
+	Array<node> v(n);
+	NodeArray<double> weights(G, 0.0);
+	double Wk = 0.0;
+
+	for (int i = 0; i < n; i++) {
+		v[i] = G.newNode();
+		weights[i] = dist(rng);
+		Wk += weights[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			double p = 1.0 - exp (-(weights[i] * weights[j]) / Wk);
+			if (dist(rng) <= p) {
+				G.newEdge(v[i], v[j]);
+			}
+		}
+	}
+
+}
+
+
+int main(){
+	//! TODO: tests
+	Graph G;
+	norrosReittuRandomWeights(G, 100);
+
+	Array<double> weights(0, 10);
+	for (double &x : weights) {
+		x = 4.0;
+	}
+
+	norrosReittu(G, 10, weights);
+
+	return 0;
+}
