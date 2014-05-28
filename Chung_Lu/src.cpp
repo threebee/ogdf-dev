7464df89c4a3a0eefb033488c8b89ef143b7f84c
Anuj Agarwal <anujagarwal464@gmail.com>
2014-05-28 22:05:52 +0530

    updated Chung-Lu graph

--- Chung_Lu/src.cpp
+++ Chung_Lu/src.cpp
@@ -1,11 +1,16 @@
-/*
-* Chung-Lu graph
-*
-* Consider Graph G (n, W) with n vertices and weights W = (W1, W2, ..., Wn) >= 0
-* In this model, an edge between two vertices i and j is independently present with probability
-* P (i, j) = min ((WiWj/ summation (Wk)), 1)
-*
-*/
+/**
+ * The average distances in random graphs with given expected degrees
+ * Link: http://www.pnas.org/content/99/25/15879.short
+ *
+ * The Similarity between Stochastic Kronecker and Chung-Lu Graph Models
+ * Link: http://arxiv.org/pdf/1110.4925.pdf
+ *
+ * Chung Lu Graph
+ * Consider Graph G (n, W) with n vertices and degree sequence W = (W1, W2, ..., Wn) >= 0
+ * In this model, an edge between two vertices i and j is independently present with probability
+ * P (i, j) = min ((WiWj/ summation (Wk)), 1)
+ *
+ */
 
 #include <iostream>
 #include <cmath>
@@ -16,71 +21,76 @@
 #include <ogdf/basic/NodeArray.h>
 #include <ogdf/basic/geometry.h>
 
-using std::cout;
 using std::minstd_rand;
 using std::uniform_int_distribution;
 using std::uniform_real_distribution;
 using namespace ogdf;
 
-//! Creates a ChungLu Graph. 
+//! Creates a ChungLu Graph.
 /**
-* @param G is assigned the generated graph.
-* @param n is the number of nodes of the generated graph.
-* @param weights is Array of weight of each node.
-*/
-void ChungLu(Graph &G, int n, Array<double> &weights) {
+ * @param G is assigned the generated graph.
+ * @param n is the number of nodes of the generated graph.
+ * @param weights is degree sequence of Graph.
+ */
+void chungLuGraph(Graph &G, int n, Array<int> &weights) {
 	G.clear();
 	if (n == 0) return;
 
 	minstd_rand rng(randomSeed());
 	uniform_real_distribution<> dist(0, 1);
 
-	Array<node> v(n);
-	double Wk = 0.0;
+	int Wk = 0;
 
-	for (int i = 0; i < n; i++) {
-		v[i] = G.newNode();
+	// adding n nodes to graph and summing degree sequence
+	for (int i = 0; i < n; i++){
+		G.newNode();
 		Wk += weights[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			double p = std::min((weights[i] * weights[j] / Wk), 1.0);
-			if (dist(rng) <= p) {
-				G.newEdge(v[i], v[j]);
+	int i, j;
+	node v, w;
+	for (i = 0, v = G.firstNode(); v; v = v->succ(), i++) {
+		for (j = 0, w = v->succ(); w; w = w->succ(), j++) {
+			double probability = std::min((weights[i] * weights[j]) / (double)Wk, 1.0);
+			if (dist(rng) < (probability + std::numeric_limits<double>::epsilon())) {
+				G.newEdge(v, w);
 			}
 		}
 	}
 
 }
 
-//! Creates a ChungLu Graph with random weights. 
+//! Creates a ChungLu Graph with random degree sequence.
 /**
-* @param G is assigned the generated graph.
-* @param n is the number of nodes of the generated graph.
-*/
-void ChungLuRandomWeights(Graph &G, int n) {
+ * @param G is assigned the generated graph.
+ * @param n is the number of nodes of the generated graph.
+ */
+void chungLuRandomWeightsGraph(Graph &G, int n) {
 	G.clear();
 	if (n == 0) return;
 
 	minstd_rand rng(randomSeed());
 	uniform_real_distribution<> dist(0, 1);
 
-	Array<node> v(n);
 	NodeArray<double> weights(G, 0.0);
 	double Wk = 0.0;
-
-	for (int i = 0; i < n; i++) {
-		v[i] = G.newNode();
-		weights[i] = dist(rng);
-		Wk += weights[i];
+	
+	// adding n nodes to graph
+	for (int i = 0; i < n; i++){
+		G.newNode();
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			double p = std::min((weights[i] * weights[j] / Wk), 1.0);
-			if (dist(rng) <= p) {
-				G.newEdge(v[i], v[j]);
+	// summing degree sequence
+	for (node v : G.nodes) {
+		weights[v] = dist(rng);
+		Wk += weights[v];
+	}
+	
+	for (node v: G.nodes) {
+		for (node  w = v->succ(); w; w = w->succ()) {
+			double probability = std::min((weights[v] * weights[w] / Wk), 1.0);
+			if (dist(rng) < (probability + std::numeric_limits<double>::epsilon())) {
+				G.newEdge(v, w);
 			}
 		}
 	}
@@ -91,14 +101,14 @@ void ChungLuRandomWeights(Graph &G, int n) {
 int main(){
 	//! TODO: tests
 	Graph G;
-	ChungLuRandomWeights(G, 100);
+	chungLuRandomWeightsGraph(G, 100);
 
-	Array<double> weights(0, 10);
-	for (double &x : weights) {
-		x = 4.0;
+	Array<int> weights(0, 10);
+	for (int &x : weights) {
+		x = 4;
 	}
 
-	ChungLu(G, 10, weights);
+	chungLuGraph(G, 10, weights);
 
 	return 0;
 }
