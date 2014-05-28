84a0360ee827b50092ced20645ff45a03b61101b
Anuj Agarwal <anujagarwal464@gmail.com>
2014-05-28 23:29:39 +0530

    updated Norros-Reittu Graph

--- Norros_Reittu/src.cpp
+++ Norros_Reittu/src.cpp
@@ -1,7 +1,9 @@
-/*
- * Norros-Reittu graph
+/**
+ * On a conditionally Poissonean graph process
+ * Link: http://citeseerx.ist.psu.edu/showciting?cid=233780  
  *
- * Consider Graph G (n, W) with n vertices and weights W = (W1, W2, ..., Wn) >= 0
+ * Norros-Reittu graph
+ * Consider Graph G (n, W) with n vertices and degree sequence W = (W1, W2, ..., Wn) >= 0
  * In this model, an edge between two vertices i and j is independently present with probability
  *             P (i, j) = 1 – exp (-WiWj/ summation (Wk))
  *
@@ -16,7 +18,6 @@
 #include <ogdf/basic/NodeArray.h>
 #include <ogdf/basic/geometry.h>
 
-using std::cout;
 using std::minstd_rand;
 using std::uniform_int_distribution;
 using std::uniform_real_distribution;
@@ -24,63 +25,69 @@ using namespace ogdf;
 
 //! Creates a Norros-Reittu Graph. 
 /**
-* @param G is assigned the generated graph.
-* @param n is the number of nodes of the generated graph.
-* @param weights is Array of weight of each node.
-*/
-void norrosReittu(Graph &G, int n, Array<double> &weights) {
+ * @param G is assigned the generated graph.
+ * @param n is the number of nodes of the generated graph.
+ * @param weights is degree sequence of Graph.
+ */
+void norrosReittuGraph(Graph &G, int n, Array<int> &weights) {
 	G.clear();
 	if (n == 0) return;
 
 	minstd_rand rng(randomSeed());
 	uniform_real_distribution<> dist(0, 1);
 
-	Array<node> v(n);
-	double Wk = 0.0;
+	int Wk = 0;
 
+	// adding n nodes to graph and summing degree sequence
 	for (int i = 0; i < n; i++) {
-		v[i] = G.newNode();
+		G.newNode();
 		Wk += weights[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			double p = 1.0 - exp(-(weights[i] * weights[j]) / Wk);
-			if (dist(rng) <= p) {
-				G.newEdge(v[i], v[j]);
+	int i, j;
+	node v, w;
+	for (i = 0, v = G.firstNode(); v; v = v->succ(), i++) {
+		for (j = 0, w = v->succ(); w; w = w->succ(), j++) {
+			double probability = 1.0 - exp(-(weights[i] * weights[j]) / (double)Wk);
+			if (dist(rng) < (probability + std::numeric_limits<double>::epsilon())) {
+				G.newEdge(v, w);
 			}
 		}
 	}
 
 }
 
-//! Creates a Norros-Reittu Graph with random weights. 
+//! Creates a Norros-Reittu Graph with random degree sequence. 
 /**
-* @param G is assigned the generated graph.
-* @param n is the number of nodes of the generated graph.
-*/
-void norrosReittuRandomWeights(Graph &G, int n) {
+ * @param G is assigned the generated graph.
+ * @param n is the number of nodes of the generated graph.
+ */
+void norrosReittuRandomWeightsGraph(Graph &G, int n) {
 	G.clear();
 	if (n == 0) return;
 
 	minstd_rand rng(randomSeed());
 	uniform_real_distribution<> dist(0, 1);
 
-	Array<node> v(n);
 	NodeArray<double> weights(G, 0.0);
 	double Wk = 0.0;
 
+	// adding n nodes to graph
 	for (int i = 0; i < n; i++) {
-		v[i] = G.newNode();
-		weights[i] = dist(rng);
-		Wk += weights[i];
+		G.newNode();
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			double p = 1.0 - exp (-(weights[i] * weights[j]) / Wk);
-			if (dist(rng) <= p) {
-				G.newEdge(v[i], v[j]);
+	// summing degree sequence
+	for (node v : G.nodes) {
+		weights[v] = dist(rng);
+		Wk += weights[v];
+	}
+
+	for (node v : G.nodes) {
+		for (node w = v->succ(); w; w = w->succ()) {
+			double probability = 1.0 - exp(-(weights[v] * weights[w]) / Wk);
+			if (dist(rng) <= (probability + std::numeric_limits<double>::epsilon())) {
+				G.newEdge(v, w);
 			}
 		}
 	}
@@ -91,14 +98,14 @@ void norrosReittuRandomWeights(Graph &G, int n) {
 int main(){
 	//! TODO: tests
 	Graph G;
-	norrosReittuRandomWeights(G, 100);
+	norrosReittuRandomWeightsGraph(G, 100);
 
-	Array<double> weights(0, 10);
-	for (double &x : weights) {
-		x = 4.0;
+	Array<int> weights(0, 10);
+	for (int &x : weights) {
+		x = 4;
 	}
 
-	norrosReittu(G, 10, weights);
+	norrosReittuGraph(G, 10, weights);
 
 	return 0;
 }
